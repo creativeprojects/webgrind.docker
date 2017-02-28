4596057ad54e6ad64968a58d792019bca55bb635
Fred <Fred@CreativeProjects.Tech>
2017-02-28 14:16:25 +0000

    Push with tag "latest"

--- build.sh
+++ build.sh
@@ -4,5 +4,7 @@ image_name=creativeprojects/webgrind
 version=1.4.0
 
 docker pull debian:jessie
-docker build -t ${image_name}:${version} ./
+docker rmi ${image_name}:latest
+docker build -t ${image_name}:${version} -t ${image_name}:latest ./
 docker push ${image_name}:${version}
+docker push ${image_name}:latest
