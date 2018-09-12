c265fa7fec642748ad89f085ffa443cbd86a3305
Fred <Fred@CreativeProjects.Tech>
2018-09-12 16:32:21 +0100

    Change to current directory in script

--- build.sh
+++ build.sh
@@ -3,7 +3,11 @@
 image_name=creativeprojects/webgrind
 version=1.5.0
 
-docker pull debian:jessie
+cd $(dirname "${0}")
+
+docker pull debian:stretch
+docker pull ${image_name}:${version}
+docker pull ${image_name}:latest
 docker rmi ${image_name}:latest
 docker build -t ${image_name}:${version} -t ${image_name}:latest ./
 docker push ${image_name}:${version}
